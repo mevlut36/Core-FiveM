9ebd90d9108f5600eb816ee63040cbf357c9ae84
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-10 01:03:18 +0200

    Update Format.cs

--- Client/Format.cs
+++ Client/Format.cs
@@ -1,4 +1,5 @@
-﻿using CitizenFX.Core;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using Core.Client;
 using LemonUI;
 using System;
@@ -14,8 +15,10 @@ namespace Core.Client
     public class Format
     {
         public ClientMain Client;
+        public ObjectPool Pool = new ObjectPool();
         public Format(ClientMain caller)
         {
+            Pool = caller.Pool;
             Client = caller;
         }
         /*
@@ -36,9 +39,9 @@ namespace Core.Client
          */
         public void SendNotif(string text)
         {
-            BeginTextCommandThefeedPost("STRING");
-            AddTextComponentString(text);
-            EndTextCommandThefeedPostTicker(true, true);
+            API.BeginTextCommandThefeedPost("STRING");
+            API.AddTextComponentSubstringPlayerName(text);
+            API.EndTextCommandThefeedPostTicker(false, true);
         }
 
         public static void CustomNotif(string message, bool blink = true, bool saveToBrief = true)
@@ -81,6 +84,14 @@ namespace Core.Client
             World.DrawMarker(markerType, position, Vector3.Zero, Vector3.Zero, new Vector3(1, 1, 1), System.Drawing.Color.FromArgb(255, 130, 0), true);
         }
 
+        public async void PlayAnimation(string animDict, string animName, int duration)
+        {
+            Function.Call(Hash.REQUEST_ANIM_DICT, animDict);
+            while (!Function.Call<bool>(Hash.HAS_ANIM_DICT_LOADED, animDict)) await BaseScript.Delay(50);
+            Game.PlayerPed.Task.ClearAllImmediately();
+            Game.PlayerPed.Task.PlayAnimation(animDict, animName, -1, duration, 50);
+        }
+
         public static async Task<string> GetUserInput(string windowTitle, string defaultText, int maxInputLength)
         {
             // THANKS vMENU
@@ -106,6 +117,72 @@ namespace Core.Client
                         break;
                 }
             }
+        }
+
+        /*
+         * Check if date of birth is in valid format
+         * 
+         * I don't want to hear any more about it
+         */
+        public bool IsValidDateFormat(string date)
+        {
+            if (date.Length != 10)
+            {
+                return false;
+            }
+
+            string[] parts = date.Split('/');
+
+            if (parts.Length != 3)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(parts[0], out int day) || !int.TryParse(parts[1], out int month) || !int.TryParse(parts[2], out int year))
+            {
+                return false;
+            }
+
+            return IsValidDate(day, month, year);
+        }
+
+        private bool IsValidDate(int day, int month, int year)
+        {
+            if (year < 1945 || year > 2015)
+            {
+                return false;
+            }
+
+            if (month < 1 || month > 12)
+            {
+                return false;
+            }
+
+            int[] maxDaysPerMonth = new int[]
+            {
+        31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+            };
+
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                maxDaysPerMonth[1] = 29;
+            }
+
+            if (day < 1 || day > maxDaysPerMonth[month - 1])
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static DisplayAttribute GetDisplayAttribute(VehicleImport vehicle)
+        {
+            var type = typeof(VehicleImport);
+            var memberInfo = type.GetMember(vehicle.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false);
+
+            return (DisplayAttribute)attributes[0];
         }
     }
 }
