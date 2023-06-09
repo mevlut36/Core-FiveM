b2d865fb2dd283e4951ec691b6be64150b494b33
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-10 01:04:58 +0200

    Create VehicleImport.cs
    Enum of vehicles import

---
+++ Client/VehicleImport.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Client
+{
+    public enum VehicleImport
+    {
+        [Display("a1gt63", "Mercedes AMG GT 63", 3500)]
+        MercedesAMGGT63,
+
+        [Display("cls63amg", "Mercedes CLS 350 AMG", 2500)]
+        CLS63AMG,
+
+        [Display("rs7", "Audi RS7", 2000)]
+        AudiRS7
+    }
+
+    public class DisplayAttribute : Attribute
+    {
+        public string VehicleName { get; }
+        public string Name { get; }
+        public int Price { get; }
+
+        public DisplayAttribute(string vehicleName, string name, int price)
+        {
+            VehicleName = vehicleName;
+            Name = name;
+            Price = price;
+        }
+    }
+
+
+}
