58b865ee6c421926cc61ad234cc7ae63477a5771
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-10 01:07:10 +0200

    Update player.sql
    Update database

--- player.sql
+++ player.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 02 juin 2023 à 23:34
+-- Généré le : ven. 09 juin 2023 à 23:06
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -31,17 +31,24 @@ DROP TABLE IF EXISTS `player`;
 CREATE TABLE IF NOT EXISTS `player` (
   `id` int NOT NULL AUTO_INCREMENT,
   `license` varchar(255) NOT NULL,
+  `bitcoin` int NOT NULL,
+  `gender` varchar(20) NOT NULL,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `clothes` text NOT NULL,
   `money` int NOT NULL DEFAULT '20000',
+  `bills` text,
+  `cars` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `phone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `inventory` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `birth` varchar(10) NOT NULL,
   `last_position` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `player`
+--
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
