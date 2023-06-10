e8f1b76d66e4eb6e22c9bad48588d172e4ea2e9c
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-11 00:50:11 +0200

    Create LTDShop.cs

---
+++ Client/LTDShop.cs
@@ -0,0 +1,239 @@
+using CitizenFX.Core;
+using LemonUI;
+using LemonUI.Menus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace Core.Client
+{
+    public class LTDShop
+    {
+        public ClientMain Client;
+        public Format Format;
+        public ObjectPool Pool = new ObjectPool();
+        public BaseScript BaseScript;
+        public PlayerMenu PlayerMenu;
+
+        List<LTDShopInfo> LTDShops = new List<LTDShopInfo>();
+        List<LTDItems> LTDItems = new List<LTDItems>();
+
+        public LTDShop(ClientMain caller)
+        {
+            Pool = caller.Pool;
+            Client = caller;
+            Format = caller.Format;
+            PlayerMenu = caller.PlayerMenu;
+
+            // LTD's ITEM
+            LTDItems item1 = new LTDItems("Pain", "N'oubliez pas de dire Bismillah avant de manger :)", 25);
+            LTDItems.Add(item1);
+
+            LTDItems item2 = new LTDItems("Eau", "N'oubliez pas de dire Bismillah avant de boire :)", 25);
+            LTDItems.Add(item2);
+
+            LTDItems item3 = new LTDItems("Phone", "Excellent téléphone", 1000);
+            LTDItems.Add(item3);
+
+            // LTD LIST
+            LTDShopInfo ltd1 = new LTDShopInfo("Strawberry", new Vector3(26.2f, -1346.9f, 29.3f), new Vector3(24.3f, -1346.6f, 29.3f), new Vector3(33.1f, -1347.9f, 29.3f));
+            LTDShops.Add(ltd1);
+
+            LTDShopInfo ltd2 = new LTDShopInfo("Davis", new Vector3(-48, -1756.8f, 29.3f), new Vector3(-46.4f, -1758.1f, 29.4f), new Vector3(-56.7f, -1752.3f, 29.3f));
+            LTDShops.Add(ltd2);
+
+            LTDShopInfo ltd3 = new LTDShopInfo("Chumash", new Vector3(-3243.2f, 1001.8f, 12.7f), new Vector3(-3243.2f, 999.9f, 12.7f), new Vector3(-3240.9f, 1008.7f, 12.8f));
+            LTDShops.Add(ltd3);
+
+            LTDShopInfo ltd4 = new LTDShopInfo("Banham Canyon", new Vector3(-3040.9f, 585.5f, 7.9f), new Vector3(-3040.9f, 583.9f, 7.9f), new Vector3(-3040.9f, 592.9f, 7.9f));
+            LTDShops.Add(ltd4);
+
+            LTDShopInfo ltd5 = new LTDShopInfo("Banham Canyon", new Vector3(-2968.3f, 390.3f, 15), new Vector3(-2966.1f, 390.3f, 15), Vector3.Zero);
+            LTDShops.Add(ltd5);
+
+            LTDShopInfo ltd6 = new LTDShopInfo("Richman Glen", new Vector3(-1820.7f, 792.6f, 138.1f), new Vector3(-1819.2f, 792.6f, 138.1f), new Vector3(-1827.1f, 785.4f, 138.2f));
+            LTDShops.Add(ltd6);
+
+            LTDShopInfo ltd7 = new LTDShopInfo("Morningwood", new Vector3(-1487.2f, -379.4f, 40.1f), new Vector3(-1485.6f, -378.1f, 40f), Vector3.Zero);
+            LTDShops.Add(ltd7);
+            
+            LTDShopInfo ltd8 = new LTDShopInfo("Monts Tataviam", new Vector3(2555.6f, 382.5f, 108.6f), new Vector3(2555.6f, 380.8f, 108.6f), new Vector3(2558.1f, 389.3f, 108.6f));
+            LTDShops.Add(ltd8);
+            
+            LTDShopInfo ltd9 = new LTDShopInfo("Sandy Shores", new Vector3(1392.3f, 3604.4f, 34.9f), new Vector3(1392.3f, 3606.4f, 34.9f), Vector3.Zero);
+            LTDShops.Add(ltd9);
+
+            LTDShopInfo ltd10 = new LTDShopInfo("Grapeseed", new Vector3(1698.1f, 4924.8f, 42), new Vector3(1696.9f, 4923.1f, 42), new Vector3(1702.7f, 4933.1f, 42));
+            LTDShops.Add(ltd10);
+            
+            LTDShopInfo ltd11 = new LTDShopInfo("Mirror Park", new Vector3(1163.4f, -323.9f, 69.2f), new Vector3(1165.2f, -323.6f, 69.2f), new Vector3(1154.1f, -326.6f, 69.2f));
+            LTDShops.Add(ltd11);
+            
+            LTDShopInfo ltd12 = new LTDShopInfo("Vinewood", new Vector3(374.1f, 325.9f, 103.5f), new Vector3(372.2f, 326.5f, 103.5f), new Vector3(380.9f, 323.6f, 103.5f));
+            LTDShops.Add(ltd12);
+            
+            LTDShopInfo ltd13 = new LTDShopInfo("Vespucci", new Vector3(-1223.5f, -907.1f, 12.3f), new Vector3(-1222.2f, -908.8f, 12.3f), Vector3.Zero);
+            LTDShops.Add(ltd13);
+            
+            LTDShopInfo ltd14 = new LTDShopInfo("Murrieta Heights", new Vector3(1136.1f, -981.7f, 46.4f), new Vector3(1133.6f, -981.7f, 46.4f), Vector3.Zero);
+            LTDShops.Add(ltd14);
+            
+            LTDShopInfo ltd15 = new LTDShopInfo("Harmony", new Vector3(547.4f, 2671.3f, 42.1f), new Vector3(549.2f, 2670.9f, 42.1f), new Vector3(540.2f, 2670.8f, 42.1f));
+            LTDShops.Add(ltd15);
+            
+            LTDShopInfo ltd16 = new LTDShopInfo("Mont Chiliad", new Vector3(1729.0f, 6414.4f, 35), new Vector3(1727.6f, 6415.3f, 35), new Vector3(1735.3f, 6411, 35));
+            LTDShops.Add(ltd16);
+
+            LTDShopInfo ltd17 = new LTDShopInfo("Mont Chiliad", new Vector3(1729.0f, 6414.4f, 35), new Vector3(1727.6f, 6415.3f, 35), new Vector3(1735.3f, 6411, 35));
+            LTDShops.Add(ltd17);
+        }
+
+        public void LTDMenu()
+        {
+            var playerCoords = GetEntityCoords(PlayerPedId(), false);
+            var items = JsonConvert.DeserializeObject<List<ItemQuantity>>(PlayerMenu.PlayerInst.Inventory);
+
+            foreach (var ltd in LTDShops)
+            {
+                var distance = GetDistanceBetweenCoords(ltd.Checkout.X, ltd.Checkout.Y, ltd.Checkout.Z, playerCoords.X, playerCoords.Y, playerCoords.Z, false);
+
+                if (distance < 4)
+                {
+                    Format.SetMarker(ltd.Checkout, MarkerType.HorizontalCircleFat);
+                }
+                if (distance < 1)
+                {
+                    Format.SendTextUI("~w~Cliquer sur ~r~E ~w~ pour ouvrir");
+
+                    if (IsControlPressed(0, 38))
+                    {
+                        var menu = new NativeMenu("LTD", $"LTD - {ltd.LTDName}")
+                        {
+                            TitleFont = CitizenFX.Core.UI.Font.ChaletLondon,
+                            UseMouse = false
+                        };
+                        Pool.Add(menu);
+                        menu.Visible = true;
+
+                        var dollarsItem = items.FirstOrDefault(item => item.Item == "Dollars");
+                        var breadItem = items.FirstOrDefault(item => item.Item == "Pain");
+                        var waterItem = items.FirstOrDefault(item => item.Item == "Eau");
+                        var phoneItem = items.FirstOrDefault(item => item.Item == "Phone");
+
+                        var myMoney = new NativeItem($"Mon argent: ~g~${dollarsItem}");
+                        foreach(var item in LTDItems)
+                        {
+                            var _ = new NativeItem($"{item.Name}", $"{item.Description}", $"~g~${item.Price}");
+                            menu.Add(_);
+                            _.Activated += async (sender, e) =>
+                            {
+                                var textInput = await Format.GetUserInput("Quantité", "1", 4);
+                                var parsedInput = Int32.Parse(textInput);
+
+                                var result = item.Price * parsedInput;
+                                if (result <= PlayerMenu.PlayerInst.Money)
+                                {
+                                    PlayerMenu.PlayerInst.Money -= result;
+                                    breadItem.Quantity += parsedInput;
+                                }
+                                else
+                                {
+                                    Format.SendNotif("~r~La somme est trop élevé");
+                                }
+                                PlayerMenu.PlayerInst.Inventory = JsonConvert.SerializeObject(items);
+                                BaseScript.TriggerServerEvent("core:transaction", result, item.Name, parsedInput);
+                            };
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ATMMenu()
+        {
+            var playerCoords = GetEntityCoords(PlayerPedId(), false);
+            var items = JsonConvert.DeserializeObject<List<ItemQuantity>>(PlayerMenu.PlayerInst.Inventory);
+
+            foreach (var ltd in LTDShops)
+            {
+                var distance = GetDistanceBetweenCoords(ltd.ATM.X, ltd.ATM.Y, ltd.ATM.Z, playerCoords.X, playerCoords.Y, playerCoords.Z, false);
+
+                if (distance < 4)
+                {
+                    Format.SetMarker(ltd.ATM, MarkerType.HorizontalCircleFat);
+                }
+                if (distance < 1)
+                {
+                    Format.SendTextUI("~w~Cliquer sur ~r~E ~w~ pour ouvrir");
+
+                    if (IsControlPressed(0, 38))
+                    {
+                        var menu = new NativeMenu("Banque", "Bienvenue")
+                        {
+                            TitleFont = CitizenFX.Core.UI.Font.ChaletLondon,
+                            UseMouse = false
+                        };
+                        Pool.Add(menu);
+                        menu.Visible = true;
+
+                        var dollarsItem = items.FirstOrDefault(item => item.Item == "Dollars");
+
+                        var myMoneyInBank = new NativeItem($"En banque: ~g~${PlayerMenu.PlayerInst.Money}", "Rien ne se crée, tout se transactionne");
+                        var myMoney = new NativeItem($"En possession: ~g~${dollarsItem.Quantity}");
+
+                        var actionBank = new NativeListItem<string>("Action", "", "~g~<b>Retirer</b>", "~g~<b>Déposer</b>");
+                        actionBank.Activated += async (sender, e) =>
+                        {
+                            var textInput = await Format.GetUserInput("Quantité", "1", 20);
+                            var parsedInput = Int32.Parse(textInput);
+
+                            if (actionBank.SelectedItem == "~g~<b>Retirer</b>")
+                            {
+                                if (parsedInput <= PlayerMenu.PlayerInst.Money)
+                                {
+                                    PlayerMenu.PlayerInst.Money -= parsedInput;
+                                    dollarsItem.Quantity += parsedInput;
+                                }
+                                else
+                                {
+                                    Format.SendNotif("~r~La somme est trop élevé");
+                                }
+                            }
+                            else if (actionBank.SelectedItem == "~g~<b>Déposer</b>")
+                            {
+                                if (parsedInput <= dollarsItem.Quantity)
+                                {
+                                    PlayerMenu.PlayerInst.Money += parsedInput;
+                                    dollarsItem.Quantity -= parsedInput;
+                                }
+                                else
+                                {
+                                    Format.SendNotif("~r~La somme est trop élevé");
+                                }
+                            }
+
+                            PlayerMenu.PlayerInst.Inventory = JsonConvert.SerializeObject(items);
+                            BaseScript.TriggerServerEvent("core:bankTransaction", actionBank.SelectedItem, parsedInput);
+                            menu.Visible = false;
+                        };
+
+                        menu.Add(myMoneyInBank);
+                        menu.Add(myMoney);
+                        menu.Add(actionBank);
+                    }
+                }
+            }
+        }
+
+
+        public void OnTick()
+        {
+            LTDMenu();
+            ATMMenu();
+        }
+    }
+}
