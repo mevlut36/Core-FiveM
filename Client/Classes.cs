3f2fbebe4b14d23dcace514d473d520edeb4723a
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-11 00:49:36 +0200

    Update Classes.cs
    Add AmmuNation, LTDs and LTD's item classes

--- Client/Classes.cs
+++ Client/Classes.cs
@@ -57,6 +57,32 @@ namespace Core.Client
         public int ColorSecondary { get; set; }
     }
 
+    public class LTDItems
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public LTDItems(string name, string description, int price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+
+    public class AmmuNationInfo
+    {
+        public string AmmuNationName { get; set; }
+        public Vector3 Checkout { get; set; }
+        public Vector3 PNJCoords { get; set; }
+        public AmmuNationInfo(string ammuNationName, Vector3 checkout, Vector3 pnjCoords)
+        {
+            AmmuNationName = ammuNationName;
+            Checkout = checkout;
+            PNJCoords = pnjCoords;
+        }
+    }
+
     public class BankInfo
     {
         public string BankName { get; set; }
@@ -68,6 +94,22 @@ namespace Core.Client
             BankName = bankName;
             Checkout = checkout;
             Robbery = robbery;
+        }
+    }
+
+    public class LTDShopInfo
+    {
+        public string LTDName { get; set; }
+        public Vector3 Checkout { get; set; }
+        public Vector3 PNJCoords { get; set; }
+        public Vector3 ATM { get; set; }
+
+        public LTDShopInfo(string ltdName, Vector3 checkout, Vector3 pnjCoords, Vector3 atm)
+        {
+            LTDName = ltdName;
+            Checkout = checkout;
+            PNJCoords = pnjCoords;
+            ATM = atm;
         }
     }
 }
