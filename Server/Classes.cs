3c4b503fad12232c2931b701aaee19fc48f93d2f
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-06-14 19:50:54 +0200

    Create Classes.cs

---
+++ Server/Classes.cs
@@ -0,0 +1,82 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Server
+{
+    public class PlayerInstance
+    {
+        public string Gender { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Bitcoin { get; set; }
+        public string Cars { get; set; }
+        public string Birth { get; set; }
+        public string Clothes { get; set; }
+        public string ClothesList { get; set; }
+        public int Money { get; set; }
+        public string Bills { get; set; }
+        public string Inventory { get; set; }
+    }
+    public class VehicleInfo
+    {
+        public string Model { get; set; }
+        public string Plate { get; set; }
+        public List<BootInfo> Boot { get; set; }
+        public int EngineLevel { get; set; }
+        public int BrakeLevel { get; set; }
+        public int ColorPrimary { get; set; }
+        public int ColorSecondary { get; set; }
+    }
+
+    public class BootInfo
+    {
+        public string Item { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Bills
+    {
+        [JsonProperty("company")]
+        public string Company { get; set; }
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+        [JsonProperty("author")]
+        public string Author { get; set; }
+        [JsonProperty("date")]
+        public string Date { get; set; }
+    }
+
+
+    public class ItemQuantity
+    {
+        [JsonProperty("item")]
+        public string Item { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("type")]
+        public string ItemType { get; set; }
+    }
+
+    public class ClothesInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("component")]
+        public int Component { get; set; }
+
+        [JsonProperty("drawable")]
+        public int Drawable { get; set; }
+
+        [JsonProperty("texture")]
+        public int Texture { get; set; }
+
+        [JsonProperty("palette")]
+        public int Palette { get; set; }
+    }
+}
